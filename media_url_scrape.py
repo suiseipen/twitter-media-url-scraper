06a0bb2c2092d24eca6b020721127314c1ca1695
suiseipen <130726616+suiseipen@users.noreply.github.com>
2023-04-14 13:24:52 +0900

    Update and rename main.py to media_url_scrape.py

---
+++ media_url_scrape.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+authorization_key = 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA'
+
+def video_url(tweet_id):
+	api_url = 'https://api.twitter.com/1.1/statuses/show/%s.json' % tweet_id
+	headers = {
+		'authorization': authorization_key,
+		'x-guest-token': guest_key()
+		}
+		
+	data = {
+		'cards_platform': 'Web-12',
+		'include_cards': 1,
+		'include_reply_count': 1,
+		'include_user_entities': 0,
+		'tweet_mode': 'extended',
+		}
+		
+	res = requests.get(api_url, data=data, headers=headers)
+	tweet_data = res.json()
+	ex_entities = tweet_data['extended_entities']["media"][0]["video_info"]["variants"][3]["url"]
+	return ex_entities
+	
